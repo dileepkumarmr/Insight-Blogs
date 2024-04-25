4b4ca7a7a38bb8337e060bbd248dc245d74e895d
dileepkumarmr <dileepmr35@gmail.com>
2024-04-25 22:50:09 +0530

    categories add/remove

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import { ImCross } from "react-icons/im";
 
 const CreatePost = () => {
+
+    const [cat,setCat]=useState("")
+    const [cats,setCats]=useState([])
+
+    const deleteCategory=(i)=>{
+        let updatedCats=[...cats]
+        updatedCats.splice(i)
+        setCats(updatedCats)
+     }
+ 
+     const addCategory=()=>{
+         let updatedCats=[...cats]
+         updatedCats.push(cat)
+         setCat("")
+         setCats(updatedCats)
+     }
+
+
+
     return (
         <div>
             <Navbar />
@@ -13,20 +32,32 @@ const CreatePost = () => {
 
                     <input type="file" className='px-4' name="" id="" />
                     <div className='flex items-center space-x-4 md:space-x-8'>
-                        <input type="text" placeholder="Enter post category" className='px-4 py-2 outline-none' id="" />
-                        <div className='bg-black text-white px-4 py-2 font-semibold cursor-pointer'>Add</div>
+                        <input
+                            value={cat}
+                            onChange={(e) => setCat(e.target.value)}
+                            type="text"
+                            placeholder="Enter post category"
+                            className='px-4 py-2 outline-none' id="" />
+                        <div
+                            onClick={addCategory}
+                            className='bg-black text-white px-4 py-2 font-semibold cursor-pointer'>
+                            Add
+                        </div>
                     </div>
 
                     {/* Categories */}
                     <div className='flex px-4 mt-3'>
-                        <div className='flex justify-center items-center space-x-2 mr-4 bg-gray-200 px-2 py-1 rounded-md'>
-                            <p>Tech</p>
-                            <p className='text-white bg-black rounded-full cursor-pointer px-1 p-1 text-sm'><ImCross /></p>
-                        </div>
-                        <div className='flex justify-center items-center space-x-2 mr-4 bg-gray-200 px-2 py-1 rounded-md'>
-                            <p>Tech</p>
-                            <p className='text-white bg-black rounded-full cursor-pointer px-1 p-1 text-sm'><ImCross /></p>
-                        </div>
+
+                        {cats?.map((c, i) => (
+                            <div key={i} className='flex justify-center items-center space-x-2 mr-4 bg-gray-200 px-2 py-1 rounded-md'>
+                                <p>{c}</p>
+                                <p
+                                    onClick={()=> deleteCategory(i)}
+                                    className='text-white bg-black rounded-full cursor-pointer px-1 p-1 text-sm'><ImCross /></p>
+                            </div>
+                        ))}
+
+
                     </div>
 
                 </form>
